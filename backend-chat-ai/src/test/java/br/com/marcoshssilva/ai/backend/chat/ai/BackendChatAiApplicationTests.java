75a904be36fa5776d68df4350b8cf26bbd029e90
SenhorDasEstrelas443 <76234459+marcoshssilva@users.noreply.github.com>
2024-09-02 20:56:44 -0300

    start project

---
+++ backend-chat-ai/src/test/java/br/com/marcoshssilva/ai/backend/chat/ai/BackendChatAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.ai.backend.chat.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChatAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
