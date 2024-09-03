9768bbcdefcaf21cce52794c84ed0021ae713813
SenhorDasEstrelas443 <76234459+marcoshssilva@users.noreply.github.com>
2024-09-02 22:06:05 -0300

    create client

---
+++ backend-chat-ai/src/main/java/br/com/marcoshssilva/ai/backend/chat/ai/internal/exceptions/ChatClientInterfaceException.java
@@ -0,0 +1,7 @@
+package br.com.marcoshssilva.ai.backend.chat.ai.internal.exceptions;
+
+public class ChatClientInterfaceException extends Exception {
+    public ChatClientInterfaceException(String message) {
+        super(message);
+    }
+}
