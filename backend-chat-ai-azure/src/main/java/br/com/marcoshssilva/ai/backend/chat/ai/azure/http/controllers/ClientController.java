893631ded15fa68172d14ab8806a08ca19af9194
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2024-09-20 23:32:55 -0300

    feat: Use models ChatClientRequest and ChatClientResponse. Then implements prompt endpoint to use POST instead GET

--- backend-chat-ai-azure/src/main/java/br/com/marcoshssilva/ai/backend/chat/ai/azure/http/controllers/ClientController.java
+++ backend-chat-ai-azure/src/main/java/br/com/marcoshssilva/ai/backend/chat/ai/azure/http/controllers/ClientController.java
@@ -1,12 +1,11 @@
 package br.com.marcoshssilva.ai.backend.chat.ai.azure.http.controllers;
 
+import br.com.marcoshssilva.ai.backend.chat.ai.azure.http.models.requests.ChatClientRequest;
+import br.com.marcoshssilva.ai.backend.chat.ai.azure.http.models.responses.ChatClientResponse;
 import br.com.marcoshssilva.ai.backend.chat.ai.azure.internal.exceptions.ChatClientInterfaceException;
 import br.com.marcoshssilva.ai.backend.chat.ai.azure.internal.services.ChatClientInterfaceService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ai")
@@ -18,7 +17,14 @@ public class ClientController {
     }
 
     @GetMapping("/prompt")
-    public ResponseEntity<String> callPromptWithSimpleUserInput(@RequestParam(value = "userInput", required = true) String userInput) throws ChatClientInterfaceException {
-        return ResponseEntity.ok(chatClientInterfaceService.callPrompt(userInput));
+    public ResponseEntity<ChatClientResponse> callPromptWithSimpleUserInputFromQueryString(@RequestParam(value = "userInput", required = true) String userInput) throws ChatClientInterfaceException {
+        ChatClientResponse chatClientResponse = new ChatClientResponse(chatClientInterfaceService.callPrompt(userInput));
+        return ResponseEntity.ok(chatClientResponse);
+    }
+
+    @PostMapping("/prompt")
+    public ResponseEntity<ChatClientResponse> callPromptWithSimpleUserInputFromJson(@RequestBody ChatClientRequest data) throws ChatClientInterfaceException {
+        ChatClientResponse chatClientResponse = new ChatClientResponse(chatClientInterfaceService.callPrompt(data.getUserInput()));
+        return ResponseEntity.ok(chatClientResponse);
     }
 }
